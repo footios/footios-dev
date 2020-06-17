4f9d689301b046252d617e4baa1f9f82eb00876b
fotis tsakiris <footios76@gmail.com>
2020-06-17 15:58:06 +0300

    Updates

---
+++ precache-manifest.701c094ee27949e8761db8199c79935f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b20178e2d6e53400f90e",
+    "url": "/static/js/main.b20178e2.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "b20178e2d6e53400f90e",
+    "url": "/static/css/main.51e6efc0.chunk.css"
+  },
+  {
+    "revision": "763492e61dbf49f388bb1206486cd40d",
+    "url": "/index.html"
+  }
+];
